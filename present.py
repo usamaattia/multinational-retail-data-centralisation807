28a4ebf8f2ace3192c25b9d5eb161829ef01772d
usamaattia <usamaalatiyah9@gmail.com>
2023-12-29 14:48:54 +0000

    adding task 3 of milestone3

--- present.py
+++ present.py
@@ -128,3 +128,46 @@ ALTER TABLE dim_users
 ALTER COLUMN join_date TYPE DATE USING TO_DATE(join_date, 'YYYY-MM-DD');
 
 '''
+
+
+
+##################################
+
+    # milestone 3 task 3
+
+'''
+UPDATE dim_store_details
+SET latitude = COALESCE(latitude, lat);
+
+ALTER TABLE dim_store_details
+ALTER COLUMN longitude TYPE FLOAT;
+
+ALTER TABLE dim_store_details
+ALTER COLUMN locality TYPE VARCHAR(255);
+
+ALTER TABLE dim_store_details
+ALTER COLUMN store_code TYPE VARCHAR(11); 
+
+ALTER TABLE dim_store_details
+ALTER COLUMN staff_numbers TYPE SMALLINT;
+
+ALTER TABLE dim_store_details
+ALTER COLUMN opening_date TYPE DATE USING TO_DATE(opening_date, 'YYYY-MM-DD');
+
+ALTER TABLE dim_store_details
+ALTER COLUMN store_type TYPE VARCHAR(255);
+
+ALTER TABLE dim_store_details
+ALTER COLUMN latitude TYPE FLOAT;
+
+ALTER TABLE dim_store_details
+ALTER COLUMN country_code TYPE VARCHAR(255);
+
+ALTER TABLE dim_store_details
+ALTER COLUMN continent TYPE VARCHAR(255);
+
+UPDATE dim_store_details
+SET locality = 'N/A'
+WHERE locality IS NULL;
+
+'''
